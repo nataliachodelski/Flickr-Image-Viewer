41c1395b6cd26791378b52649164a902321ab063
Natalia <nchodelski@gmail.com>
2016-03-31 23:38:03 -0700

    Initial Commit

---
+++ Flickr Images/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flickr Images
+//
+//  Created by Natalia on 31/03/2016.
+//  Copyright © 2016 NataliaDeveloper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
